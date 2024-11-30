467616736a3dc371369791ef8bfe0615900c4144
Bia <agiampedro@gmail.com>
2024-11-30 17:47:29 -0300

    :sparkles: feat: Adiciona função para os finais serem aleatórios

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 // Estrutura de um nó da árvore
 typedef struct Node {
@@ -20,6 +21,28 @@ Node* createNode(const char *desc) {
     return node;
 }
 
+// Função que gera fins diferentes
+const char* getRandomEnding() {
+    const char* endings[] = {
+        "Voce encontrou uma saida secreta e conseguiu se salvar! (Fim)",
+        "Voce encontrou uma porta e conseguiu sair do labirinto! (Fim)",
+        "Voce encontrou um poço dos desejos e fez um pedido de sair do labirinto! (Fim)",
+        "Voce caiu em uma armadilha perigosa e morreu! (Fim)",
+        "Voce encontrou uma armadilha de areia movediça e morreu! (Fim)",
+       "Voce foi atingido por flechas vindas do teto e morreu! (Fim)",
+        "Voce foi atacado por um monstro e morreu! (Fim)",
+        "Voce encontrou um portal para outro labirinto! (Fim)",
+        "Voce caiu em um poco de lava e morreu! (Fim)",
+        "Voce foi capturado por guardioes do labirinto que te comeram vivo! (Fim)",
+        "Voce tropecou em um bau vazio... azar! (Fim)",
+        "Voce foi teletransportado para o inicio do labirinto!",
+        "Voce caiu em um buraco sem fim! (Fim)"
+        
+    };
+    int index = rand() % 13; 
+    return endings[index];
+}
+
 // Função cria o labirinto
 Node* createRoom(int layer, int qntLayers) {
     char description[200];
@@ -32,13 +55,14 @@ Node* createRoom(int layer, int qntLayers) {
         room->center = createRoom(layer + 1, qntLayers);   
         room->right = createRoom(layer + 1, qntLayers);    
     } else {
-        room->left = createNode("Voce encontrou uma saida secreta! (Fim)");
-        room->center = createNode("Voce caiu em uma armadilha! (Fim)");
-        room->right = createNode("Voce encontrou um bau de tesouro! (Fim)");
+        room->left = createNode(getRandomEnding());
+        room->center = createNode(getRandomEnding());
+        room->right = createNode(getRandomEnding());
     }
 
     return room;
 }
+
 
 // Função para liberar a memória da árvore
 void freeMaze(Node* node) {
@@ -51,7 +75,7 @@ void freeMaze(Node* node) {
 }
 
 // Função para depuração (remover quando estiver tudo pronto)
-void printMaze(Node* node, int layer, int qntLayers) {
+void printMaze(Node* node, int layer) {
     if (node == NULL) {
         return; 
     }
@@ -74,11 +98,13 @@ void printMaze(Node* node, int layer, int qntLayers) {
 }
 
 int main() {
+    srand(time(NULL));
+
     // Constrói o labirinto de acordo com o número de camadas passadas no parâmetro;
     Node *maze = createRoom(1, 3); 
 
     printf("Estrutura do Labirinto:\n");
-    printMaze(maze, 1, 3);
+    printMaze(maze, 1);
 
     freeMaze(maze);
 
